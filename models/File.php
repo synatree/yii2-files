26b7bc5a51f132321fff0120fdc197b3310548f7
David Baltusavich <david@synatree.com>
2022-05-26 16:07:52 -0400

    ...

--- models/File.php
+++ models/File.php
@@ -324,7 +324,7 @@ class File extends ActiveRecord
         {
             // we don't' have this file in our directory yet
             $parts = explode('.', basename($this->filename_user));
-            $ext = $parts[-1];
+            $ext = end($parts);
             $name = implode(".", array_slice($parts,0,-1));
             $uniq = uniqid($name."-");
             $target = Yii::$app->getModule('files')->uploadPath . "/{$uniq}.{$ext}";
