4d0d1bda381a0218f9db02765c82a0083e3ef119
David Baltusavich <david@synatree.com>
2022-09-20 17:10:28 -0400

    ...

--- models/File.php
+++ models/File.php
@@ -73,9 +73,9 @@ class File extends ActiveRecord
         return Html::a($innerHtml, $this->downloadUrl($raw), ['data-pjax' => '0']);
     }
 
-    public function downloadUrl($raw = false)
+    public function downloadUrl($raw = false, $inline=false)
     {
-        return Url::to(['//files/file/download', 'id' => $this->slug, 'raw' => $raw], true);
+        return Url::to(['//files/file/download', 'id' => $this->slug, 'raw' => $raw, 'inline' => $inline], true);
     }
 
     public function deleteUrl(){
